19987ca0207ecd2e1bde1d709207d3b702f0c407
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-09-03 16:14:18 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PinkFlamingo::Application.config.secret_token = 'b759ce76e007785cdef005a0f7b487f2c995891a43f46cea5f6405e53f5c6220bb0df1587236300ed7ffab93872515c89e479dbae33d035f7bd0da98ee9bdf74'
